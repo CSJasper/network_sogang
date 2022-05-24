c248a5989c873293dc3cddf3fa1148b0246458cf
AlphaKang <jasper695@icloud.com>
2022-05-25 01:02:44 +0900

    <TODO> implement graph class some error handling don't have to be cared

--- hw2/hw2/distvec_20171248.cc
+++ hw2/hw2/distvec_20171248.cc
@@ -8,6 +8,7 @@
 #include <cstring>
 #include <cerrno>
 #include <vector>
+#include <algorithm>
 #include <unordered_set>
 #include <unordered_map>
 
@@ -34,9 +35,9 @@ typedef struct _edge {
 class graph {
 private:
 	size_t node_num;
+	size_t edge_num;
 	std::unordered_map<unsigned, vertex_t> v_map;
-	
-
+	std::unordered_map<unsigned*, edge_t> e_map;
 	~graph(void);
 
 public:
@@ -78,9 +79,23 @@ int main(int argc, char* argv[]) {
 }
 
 graph::graph(FILE* state) {
-	int _num = fscanf(state, "%zu", &(this->node_num));
+	int _num = fscanf(state, "%zu", &(this->edge_num));
 
-	for (size_t i = 0; i < this->node_num; i++) {
+	for (size_t i = 0; i < this->edge_num; i++) {
+		unsigned from = 0, to = 0;
+		int cost = 0;
+		_num = fscanf(state, "%u %u %d", &from, &to, &cost);
+		unsigned e[2] = { std::min(from, to), std::max(from, to) };
+		// 이 방법은 같은 edge가 들어왔을 때 unordered_map의 상태를 정의하지 않습니다. 그러므로 input에서 똑같은 입력을 하지 않는다고 가정합니다. (이 부분에 대한 error handling은 아직 구현되지 않았습니다.)
+		edge_t edge;
+		edge.ordered_vertices[0] = e[0];
+		edge.ordered_vertices[1] = e[1];
+		edge.cost = cost;
+		this->e_map[e] = edge;
+	}
+
+	for (std::unordered_map<unsigned*, edge_t>::iterator iter = this->e_map.begin(); iter != e_map.end(); iter++) {
+		unsigned* p = iter->first;
 
 	}
 
