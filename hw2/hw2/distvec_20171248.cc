d01c2fa7570e205e8fd9bb5fab3cdbf15c20af8d
jasper695 <jasper695@icloud.com>
2022-05-25 15:13:19 +0900

    <TODO> implement update_table method (need to test with data TDP)

--- hw2/hw2/distvec_20171248.cc
+++ hw2/hw2/distvec_20171248.cc
@@ -16,6 +16,11 @@
 #define str(s) #s
 #define print(x) printf("%s\n", str(x))
 
+// use when contain method of unordered set is not supported
+#define CONTAINS(hashable, element) (hashable.find(element) != hashable.end())
+
+#define DEBUG
+
 enum {
 	TOP_PATH = 1,
 	MSG_PATH = 2,
@@ -23,41 +28,47 @@ enum {
 };
 
 typedef struct _vertex {
-	unsigned id;
-	std::vector<unsigned> neighbor;
+	int id;
+	std::vector<std::pair<int, int>> neighbor_cost;
 }vertex_t;
 
-typedef struct _edge {
-	unsigned ordered_vertices[2];
+typedef struct _entry {
+	int next;
 	int cost;
-}edge_t;
+}entry_t;
 
 class graph {
 private:
 	size_t node_num;
 	size_t edge_num;
-	std::unordered_map<unsigned, vertex_t> v_map;
-	std::unordered_map<unsigned*, edge_t> e_map;
+
+	int last_node_id;
+
+	std::vector<int> v_list;
+	std::unordered_map<int, vertex_t> v_map;
+	std::vector<std::vector<entry_t>> tables;  // nodes의 id는 continuous 하다는 것이 보장되었기 때문에 배열을 사용하는 것이 해시 맵을 사용하는 것 보다 조금 더 유리하다.
+
 	~graph(void);
 
 public:
 	graph(FILE* state);
 
 	size_t get_node_num(void);
+	size_t get_edge_num(void);
 
+	void initialize_distvec(void);
+
+	void update_table(entry_t& source, entry_t& direct_nbd);
 
 };
 
-class distvec {
-private:
-
-public:
-};
 
 int main(int argc, char* argv[]) {
+#ifndef DEBUG
 	if (argc != 4) {
 		print(usage: distvec topologyfile messagesfile changesfile);
 	}
+#endif
 	FILE* topology = fopen((const char*)argv[TOP_PATH], "r");
 	FILE* msg = fopen((const char*)argv[MSG_PATH], "r");
 	FILE* changes = fopen((const char*)argv[CHANGES_PATH], "r");
@@ -81,24 +92,60 @@ int main(int argc, char* argv[]) {
 graph::graph(FILE* state) {
 	int _num = fscanf(state, "%zu", &(this->edge_num));
 
-	for (size_t i = 0; i < this->edge_num; i++) {
-		unsigned from = 0, to = 0;
+	std::unordered_set<int> discovered;
+
+	this->last_node_id = 0;
+
+	for (size_t i = 0; i < edge_num; i++) {
+		int v1 = 0, v2 = 0;
 		int cost = 0;
-		_num = fscanf(state, "%u %u %d", &from, &to, &cost);
-		unsigned e[2] = { std::min(from, to), std::max(from, to) };
-		// 이 방법은 같은 edge가 들어왔을 때 unordered_map의 상태를 정의하지 않습니다. 그러므로 input에서 똑같은 입력을 하지 않는다고 가정합니다. (이 부분에 대한 error handling은 아직 구현되지 않았습니다.)
-		edge_t edge;
-		edge.ordered_vertices[0] = e[0];
-		edge.ordered_vertices[1] = e[1];
-		edge.cost = cost;
-		this->e_map[e] = edge;
+		_num = fscanf(state, "%d %d %d", &v1, &v2, &cost);
+
+		this->last_node_id = std::max({ this->last_node_id, v1, v2 });
+
+		vertex_t v_first, v_second;
+		std::pair<int, int> v_fcost = { v2, cost };
+		std::pair<int, int> v_scost = { v1, cost };
+
+		if (!CONTAINS(discovered, v1)) {
+			discovered.insert(v1);
+		}
+		if (!CONTAINS(discovered, v2)) {
+			discovered.insert(v2);
+		}
+
+		if (CONTAINS(this->v_map, v1)) {
+			v_first = this->v_map[v1];
+			v_first.neighbor_cost.push_back(v_fcost);
+		}
+		else {
+			v_first.id = v1;
+			v_first.neighbor_cost.push_back(v_scost); // code duplication? (please test this)
+			this->v_map[v1] = v_first;
+		}
+
+		if (CONTAINS(this->v_map, v2)) {
+			v_second = this->v_map[v2];
+			v_second.neighbor_cost.push_back(v_scost);
+		}
+		else {
+			v_second.id = v2;
+			v_second.neighbor_cost.push_back(v_scost);
+			this->v_map[v2] = v_second;
+		}
 	}
 
-	for (std::unordered_map<unsigned*, edge_t>::iterator iter = this->e_map.begin(); iter != e_map.end(); iter++) {
-		unsigned* p = iter->first;
+	this->node_num = this->v_map.size() / 2;
+	assert(this->v_map.size() % 2 == 0);
 
+	for (size_t i = 0; i < node_num; i++) {
+		std::vector<entry_t> etry;
+		for (size_t j = 0; j < node_num; j++) {
+			entry_t e = { -1, -1 };
+			etry.push_back(e);
+		}
+		this->tables.push_back(etry);
 	}
-
 }
 
 graph::~graph(void) {
@@ -107,4 +154,22 @@ graph::~graph(void) {
 
 size_t graph::get_node_num(void) {
 	return this->node_num;
+}
+
+size_t graph::get_edge_num(void) {
+	return this->edge_num;
+}
+
+void graph::initialize_distvec(void) {
+	for (int i = 0; i <= this->last_node_id; i++) {
+		std::vector<std::pair<int, int>> current_nbd_cost = this->v_map[i].neighbor_cost;
+		for (size_t j = 0; j < current_nbd_cost.size(); j++) {
+			this->tables[i][current_nbd_cost[j].first].cost = current_nbd_cost[j].second;
+			this->tables[i][current_nbd_cost[j].first].next = current_nbd_cost[j].first;
+		}
+	}
+}
+
+void graph::update_table(entry_t& source, entry_t& direct_nbd) {
+
 }
